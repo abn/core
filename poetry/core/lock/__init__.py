c4b09bacfc90e87530b7c96a46b3d3dc3144ec1f
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2020-09-22 17:06:17 +0200

    locker: port base implementation from poetry

---
+++ poetry/core/lock/__init__.py
@@ -0,0 +1,8 @@
+from poetry.core.lock.categories import LockCategory
+from poetry.core.lock.locker import Locker
+
+
+__all__ = [
+    LockCategory.__name__,
+    Locker.__name__,
+]
--- poetry/core/poetry.py
+++ poetry/core/poetry.py
@@ -3,23 +3,26 @@ from __future__ import unicode_literals
 
 from typing import TYPE_CHECKING
 from typing import Any
+from typing import Optional
 
 from poetry.core.pyproject import PyProjectTOML
 from poetry.core.utils._compat import Path  # noqa
 
 
 if TYPE_CHECKING:
+    from poetry.core.lock.locker import Locker
     from poetry.core.packages import ProjectPackage  # noqa
     from poetry.core.pyproject.toml import PyProjectTOMLFile  # noqa
 
 
 class Poetry(object):
     def __init__(
-        self, file, local_config, package,
-    ):  # type: (Path, dict, "ProjectPackage") -> None
+        self, file, local_config, package, locker=None
+    ):  # type: (Path, dict, "ProjectPackage", Optional["Locker"]) -> None
         self._pyproject = PyProjectTOML(file)
         self._package = package
         self._local_config = local_config
+        self._locker = locker
 
     @property
     def pyproject(self):  # type: () -> PyProjectTOML
@@ -36,6 +39,16 @@ class Poetry(object):
     @property
     def local_config(self):  # type: () -> dict
         return self._local_config
+
+    @property
+    def locker(self):  # type: () -> Optional["Locker"]
+        if self._locker is None:
+            from poetry.core.lock.locker import Locker
+
+            self._locker = Locker(
+                self.pyproject.file / "poetry.lock", self.local_config
+            )
+        return self._locker
 
     def get_project_config(self, config, default=None):  # type: (str, Any) -> Any
         return self._local_config.get("config", {}).get(config, default)
